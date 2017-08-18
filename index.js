6134e839149aa9602ef13b40f0b78acc489d1631
gpciancio <gopalciancio@gmail.com>
2017-08-18 15:36:22 -0600

    test url

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ var apiServerHost = process.env.apiServerHost;
 
 var app = express();
 
+var api_key = process.env.api_key;
+
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -11,8 +14,12 @@ app.use(function(req, res, next) {
 });
 
 app.use('/', function(req, res) {
-  var url = apiServerHost + req.url;
-https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=your_key&per_page=10&format=json&nojsoncallback=1
+
+  let key = (req.url.indexOf(‘?’) !== -1) ? ‘&’ : ‘?’;
+  key += ‘api_key=’ + process.env.api_key;
+
+ var url = apiServerHost + req.url + key;
+//https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=your_key&per_page=10&format=json&nojsoncallback=1
   req.pipe(request(url)).pipe(res);
 });
 
